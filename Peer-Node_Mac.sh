2f70060941f321a086db151dec3dd14c847a416e
17akash <you@example.com>
2022-02-01 14:08:49 +0530

    upated installer

--- Peer-Node_Mac.sh
+++ Peer-Node_Mac.sh
@@ -11,7 +11,7 @@ cd peer-node-installer
 
 git checkout Mac_Binary
 
-sudo chmod +x substrate_mac
+sudo chmod +x peer
 
 
 echo "Enter Your Node Name: "
@@ -20,4 +20,4 @@ echo "Enter Your Node Name: "
 read x
   
 
-sudo ./substrate_mac   --base-path /tmp/"${x}" --chain ./customSpecRaw.json   --port 30333  --ws-external   --rpc-external --rpc-cors all  --no-telemetry  --validator   --rpc-methods Unsafe   --name "${x}" --bootnodes /ip4/3.109.243.150/tcp/30333/p2p/12D3KooWQn9w8AdBSBkUkAkSjK4TDcZT8LB2pfT8FUr1CAqsfBtE
+sudo ./peer   --base-path /tmp/"${x}" --chain ./customSpecRaw.json   --port 30333  --ws-external   --rpc-external --rpc-cors all  --no-telemetry  --validator   --rpc-methods Unsafe   --name "${x}" --bootnodes /ip4/3.109.243.150/tcp/30333/p2p/12D3KooWQn9w8AdBSBkUkAkSjK4TDcZT8LB2pfT8FUr1CAqsfBtE
